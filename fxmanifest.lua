05a9da432bb41558575a1e5249905a3a0db14517
Laugh <70723697+xLaugh@users.noreply.github.com>
2025-09-29 12:08:53 +0200

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,40 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
-author 'lfScripts, xLaugh, Firgyy'
-lua54 'yes'
+author 'LFScripts, xLaugh, Firgyy'
 version '0.0.1'
-escrow_ignore {
-    'config.lua',
-    'client.lua',
-    'server.lua',
-    'RageUI/RMenu.lua',
-    'RageUI/menu/RageUI.lua',
-    'RageUI/menu/Menu.lua',
-    'RageUI/menu/MenuController.lua',
-    'RageUI/components/*.lua',
-    'RageUI/menu/elements/*.lua',
-    'RageUI/menu/items/*.lua',
-    'RageUI/menu/panels/*.lua',
-    'RageUI/menu/windows/*.lua',
-}
 
 shared_scripts {
-    '@es_extended/imports.lua',
-    'config.lua'
+    'config.lua',
+    'lang.lua'
 }
 
 client_scripts {
-    '@es_extended/locale.lua',
-    'RageUI/RMenu.lua',
-    'RageUI/menu/RageUI.lua',
-    'RageUI/menu/Menu.lua',
-    'RageUI/menu/MenuController.lua',
-    'RageUI/components/*.lua',
-    'RageUI/menu/elements/*.lua',
-    'RageUI/menu/items/*.lua',
-    'RageUI/menu/panels/*.lua',
-    'RageUI/menu/windows/*.lua',
-    'client.lua',
+    'client.lua'
 }
 
 server_scripts {
